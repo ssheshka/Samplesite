f0590d56e60dc4925e506c2d7f894866f7f76060
Siarhei <siarhei.sheshka@gmail.com>
2022-04-06 07:43:22 +0300

    Add Bb Add template

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Bb
+admin.site.register(Bb)
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Bb
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
-
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
